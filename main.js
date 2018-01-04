9b796321f685cd0135700c73485d0fd62b4dec51
Maxim Korinets <mkorinets@gmail.com>
2018-01-04 18:02:46 +0700

    Fix tv power command

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const lirc = require('lirc_node');
 
 lirc.init();
 console.log(lirc.remotes);
-lirc.irsend.send_once("tv", "power", () => {
+lirc.irsend.send_once("tv", "key_power", () => {
     console.log("tv power command sent");
 });
