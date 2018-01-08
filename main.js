649c8ed93409976ddca8ad23668e97553692c73d
Maxim Korinets <mkorinets@gmail.com>
2018-01-08 14:34:39 +0700

    Remove direct rule instance

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const lirc = require('lirc_node');
-const {RecurrenceRule, scheduleJob} = require('node-schedule');
+const {scheduleJob} = require('node-schedule');
 const {HOUR=12, MINUTE=0} = process.env;
 
 
 lirc.init();
-const rule = new RecurrenceRule();
-rule.hour = HOUR;
-rule.minute = MINUTE;
 
-scheduleJob(rule, () => {
+const job = scheduleJob({hour: HOUR, minute: MINUTE}, () => {
     lirc.irsend.send_once("tv", "key_power", () => {
-        console.log("tv power command sent")
+        console.log("tv power command sent");
     });
 });
-console.log("job scheduled");
+console.log(`job scheduled: ${JSON.stringify(job)}`);
